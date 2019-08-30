553f3a642484c9e5e8df704c3f7eb77c18753ed3
Tema Methodist Diocese <methchtemaghdiocese@gmail.com>
2019-08-30 00:21:18 +0000

    Removed unused imports in Main entry

--- src/main/java/com/ectools/challenge/ectoolsimporter/EctoolsImporterApplication.java
+++ src/main/java/com/ectools/challenge/ectoolsimporter/EctoolsImporterApplication.java
@@ -1,15 +1,7 @@
 package com.ectools.challenge.ectoolsimporter;
 
-import com.ectools.challenge.ectoolsimporter.models.ProductMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
-import org.springframework.jms.support.converter.MessageConverter;
-import org.springframework.jms.support.converter.MessageType;
-import org.springframework.jms.support.converter.SimpleMessageConverter;
 
 /**
  * Main entry to application
